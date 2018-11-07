13b34cd26c2077885a92ff1a0c7d04e5bec3b5b2
John Turner <ubik642@gmail.com>
2018-11-07 10:46:21 -0500

    finished fourth button

--- file.js
+++ file.js
@@ -10,3 +10,9 @@ function strangerHello() {
   var p = document.getElementById("hello");
   p.innerHTML="Hello World!";
 }
+
+function button4Hello() {
+  var name = prompt("Put Your Name Here");
+  var p = document.getElementById("hello1");
+  p.innerHTML="Hello, " + name;
+}
